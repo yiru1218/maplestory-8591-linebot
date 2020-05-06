8bd1142bc7df370e8d5678c0fa4e2451418a4637
YiRu <mousechen24@gmail.com>
2020-05-07 05:13:47 +0800

    make it better

--- clock.py
+++ clock.py
@@ -1,11 +1,18 @@
-
+# APScheduler 讓我們的免費 dyno 在快要睡著的時候自己喚醒自己
 from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+import urllib
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
 def scheduled_job():
+    print('========== APScheduler CRON =========')
+    # 利用datetime查詢時間
+    print(f'{datetime.datetime.now().ctime()}')
+    print('========== APScheduler CRON =========')
+
     url = "https://maplestory8591.herokuapp.com/"
     conn = urllib.request.urlopen(url)
         
@@ -14,4 +21,3 @@ def scheduled_job():
 
 
 sched.start()
-
